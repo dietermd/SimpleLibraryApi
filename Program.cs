cdc99522bd26bd49ee6ffce33157a605a8ad3f1f
Dieter Mutiz <dieter_md@hotmail.com>
2023-03-26 17:30:24 -0300

    JWT authentication setup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SimpleLibraryApi.Application.Behaviors;
 using SimpleLibraryApi.Application.Middlewares;
 using SimpleLibraryApi.Endpoints.AuthorEndpoint;
@@ -10,13 +13,63 @@ using SimpleLibraryApi.Endpoints.BookEndpoint.Commands.Validators;
 using SimpleLibraryApi.Endpoints.UserEndpoint;
 using SimpleLibraryApi.Endpoints.UserEndpoint.Commands.Validators;
 using SimpleLibraryApi.Models.Context;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add JWT configuration
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+var securityScheme = new OpenApiSecurityScheme()
+{
+    Name = "Authorization",
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "JSON Web Token based security",
+};
+var securityReq = new OpenApiSecurityRequirement()
+{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[] {}
+    }
+};
+builder.Services.AddSwaggerGen(x =>
+{
+    x.AddSecurityDefinition("Bearer", securityScheme);
+    x.AddSecurityRequirement(securityReq);
+});
 
 // Db
 builder.Services.AddDbContext<ApiDbContext>(options => options.UseInMemoryDatabase("InMemory"));
@@ -36,8 +89,6 @@ builder.AddUserCommandValidators()
 // Middleware
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -52,6 +103,9 @@ if (app.Environment.IsDevelopment())
         scope.ServiceProvider.GetRequiredService<ApiDbContext>().Database.EnsureCreated();
     }
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Middleware
 app.UseMiddleware<ExceptionHandlingMiddleware>();
